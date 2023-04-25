d54301b07d8f9437166aa5e3d1cbe11928019053
Victor Franco <vfranco.itc@gmail.com>
2023-04-24 22:41:37 -0600

    Fixed scikit array functionality in test

--- Project2/prueba.py
+++ Project2/prueba.py
@@ -1,5 +1,5 @@
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
-u = np.array([1,2,3,4])
-v = np.array([1,2,3,4])
+u = np.array([[1,2,3,4]])
+v = np.array([[1,2,3,4]])
 print(cosine_similarity(u,v))
